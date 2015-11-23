9b39197597f4896c038773cdf76cc491a85b495a
Miray Kurtay <miray@tinbee.com>
2015-11-23 17:05:36 -0500

    Initial setup complete

---
+++ src/js/app.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+  $("#main-slideshow").owlCarousel({
+    navigation : false, // Show next and prev buttons
+    autoPlay: true,
+    stopOnHover: true,
+    autoHeight: true,
+    slideSpeed : 300,
+    paginationSpeed : 400,
+    singleItem:true
+  });
+
+});
